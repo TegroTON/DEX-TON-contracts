2f8192b801f2aa6c877663374025b402b2faed06
Anton F <imantonmeep@gmail.com>
2022-07-10 16:56:38 +0200

    Fix spec

--- backend/src/main/kotlin/money/tegro/dex/dto/ExchangePairDTO.kt
+++ backend/src/main/kotlin/money/tegro/dex/dto/ExchangePairDTO.kt
@@ -5,33 +5,33 @@ import org.ton.bigint.BigInt
 
 @Schema(name = "Pair", description = "Information about an exchange pair")
 data class ExchangePairDTO(
-    @get:Schema(description = "Unix timestamp of the last time data was updated")
+    @field:Schema(description = "Unix timestamp of the last time data was updated")
     val updated: Long,
 
-    @get:Schema(description = "Address of the exchange pair, base64, bounceable")
+    @field:Schema(description = "Address of the exchange pair, base64, bounceable")
     val address: String,
 
-    @get:Schema(description = "Full name of the first currency")
+    @field:Schema(description = "Full name of the first currency")
     val leftName: String,
 
-    @get:Schema(description = "Symbol, ticker, of the first currency")
+    @field:Schema(description = "Symbol, ticker, of the first currency")
     val leftSymbol: String,
 
-    @get:Schema(description = "Address of the left currency, null in case of TON; base64url, bounceable")
+    @field:Schema(description = "Address of the left currency, null in case of TON; base64url, bounceable")
     val leftAddress: String?,
 
-    @get:Schema(description = "Reserved amount in the liquidity pool for the left currency")
+    @field:Schema(description = "Reserved amount in the liquidity pool for the left currency", type = "integer")
     val leftReserved: BigInt,
 
-    @get:Schema(description = "Full name of the second currency")
+    @field:Schema(description = "Full name of the second currency")
     val rightName: String,
 
-    @get:Schema(description = "Symbol, ticker, of the second currency")
+    @field:Schema(description = "Symbol, ticker, of the second currency")
     val rightSymbol: String,
 
-    @get:Schema(description = "Address of the right currency, base64url, bounceable")
+    @field:Schema(description = "Address of the right currency, base64url, bounceable")
     val rightAddress: String,
 
-    @get:Schema(description = "Reserved amount in the liquidity pool for the right jetton")
+    @field:Schema(description = "Reserved amount in the liquidity pool for the right jetton", type = "integer")
     val rightReserved: BigInt,
 )
--- backend/src/main/kotlin/money/tegro/dex/dto/JettonDTO.kt
+++ backend/src/main/kotlin/money/tegro/dex/dto/JettonDTO.kt
@@ -4,15 +4,15 @@ import io.swagger.v3.oas.annotations.media.Schema
 
 @Schema(name = "Jetton", description = "Information about a specific jetton")
 data class JettonDTO(
-    @get:Schema(description = "Unix timestamp of the last time data was updated")
+    @field:Schema(description = "Unix timestamp of the last time data was updated")
     val updated: Long,
 
-    @get:Schema(description = "Full name of the jetton")
+    @field:Schema(description = "Full name of the jetton")
     val name: String,
 
-    @get:Schema(description = "Symbol, ticker of the jetton")
+    @field:Schema(description = "Symbol, ticker of the jetton")
     val symbol: String,
 
-    @get:Schema(description = "Jetton contract address; Always base64url, bounceable")
+    @field:Schema(description = "Jetton contract address; Always base64url, bounceable")
     val address: String,
 )
