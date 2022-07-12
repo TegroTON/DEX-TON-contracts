cda9627c980be69586d301279138f1b8b15ca469
Anton F <imantonmeep@gmail.com>
2022-07-12 17:13:10 +0200

    On-chain jetton data

--- backend/src/main/kotlin/money/tegro/dex/contract/JettonMetadata.kt
+++ backend/src/main/kotlin/money/tegro/dex/contract/JettonMetadata.kt
@@ -7,7 +7,8 @@ import io.ktor.client.statement.*
 import mu.KLogging
 import net.logstash.logback.argument.StructuredArguments.v
 import org.ton.cell.Cell
-import org.ton.smartcontract.SnakeDataTail
+import org.ton.crypto.sha256
+import org.ton.smartcontract.*
 import org.ton.tlb.loadTlb
 
 data class JettonMetadata(
@@ -27,29 +28,45 @@ data class JettonMetadata(
             content: Cell,
             httpClient: HttpClient
         ): JettonMetadata {
-            val cs = content.beginParse()
-            return when (val contentLayout = cs.loadUInt(8).toInt()) {
-                0x00 -> {
+            val full = content.beginParse().loadTlb(FullContent)
+            return when (full) {
+                is FullContent.OnChain -> {
                     logger.debug { "on-chain content layout, frick" }
-                    TODO("FUCKFUCKFUCK")
+                    val entries = full.data.toMap().mapKeys { it.key.toByteArray() }
+
+                    JettonMetadata(
+                        uri = entries.get(sha256("uri".toByteArray()))?.flatten()
+                            ?.decodeToString(), // TODO: Semi-on-chain content
+                        name = entries.get(sha256("name".toByteArray()))?.flatten()?.decodeToString(),
+                        description = entries.get(sha256("description".toByteArray()))?.flatten()?.decodeToString(),
+                        image = entries.get(sha256("image".toByteArray()))?.flatten()?.decodeToString(),
+                        imageData = entries.get(sha256("image_data".toByteArray()))?.flatten(),
+                        symbol = entries.get(sha256("symbol".toByteArray()))?.flatten()?.decodeToString(),
+                        decimals = entries.get(sha256("decimals".toByteArray()))?.flatten()?.decodeToString()?.toInt()
+                            ?: 9
+                    )
                 }
-                0x01 -> {
+                is FullContent.OffChain -> {
                     logger.debug { "off-chain content layout, thank's god" }
 
-                    val rawData =
-                        cs.loadTlb(SnakeDataTail.tlbCodec()) // TODO: Only tails work for now but usually it's good enough
-                    cs.endParse()
 
-                    val url = String(rawData.bits.toByteArray())
+                    val url = String(full.uri.data.flatten())
                     logger.debug("content url is {}", v("url", url))
 
                     val body = httpClient.get(url).bodyAsText()
                     mapper.readValue(body, JettonMetadata::class.java)
                 }
-                else -> {
-                    throw Exception("unknown content layout $contentLayout, cannot proceed")
-                }
             }
         }
     }
+}
+
+fun SnakeData.flatten(): ByteArray = when (this) {
+    is SnakeDataTail -> bits.toByteArray()
+    is SnakeDataCons -> bits.toByteArray() + next.flatten()
+}
+
+fun ContentData.flatten(): ByteArray = when (this) {
+    is ContentData.Snake -> this.data.flatten()
+    is ContentData.Chunks -> TODO("chunky content data")
 }
