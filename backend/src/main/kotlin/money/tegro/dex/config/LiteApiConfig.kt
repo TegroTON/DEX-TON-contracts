52a3f91dd3cb60d42a9fcd283a5780d92a0e56b7
Anton F <imantonmeep@gmail.com>
2022-07-14 11:51:34 +0200

    Immutable configuration for liteapi

--- backend/src/main/kotlin/money/tegro/dex/config/LiteApiConfig.kt
+++ backend/src/main/kotlin/money/tegro/dex/config/LiteApiConfig.kt
@@ -1,10 +1,16 @@
 package money.tegro.dex.config
 
 import io.micronaut.context.annotation.ConfigurationProperties
+import io.micronaut.core.bind.annotation.Bindable
 
 @ConfigurationProperties("money.tegro.dex.liteapi")
-class LiteApiConfig {
-    var ipv4 = 1959448750
-    var port = 51281
-    var key = "hyXd2d6yyiD/wirjoraSrKek1jYhOyzbQoIzV85CB98="
+interface LiteApiConfig {
+    @get:Bindable(defaultValue = "\${LITEAPI_IPV4:1959448750}")
+    val ipv4: Int
+
+    @get:Bindable(defaultValue = "\${LITEAPI_PORT:51281}")
+    val port: Int
+
+    @get:Bindable(defaultValue = "\${LITEAPI_KEY:`hyXd2d6yyiD/wirjoraSrKek1jYhOyzbQoIzV85CB98=`}")
+    val key: String
 }
