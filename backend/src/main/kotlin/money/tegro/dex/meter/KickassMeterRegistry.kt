e190cdd2d2c991c18499ca7f85545616b1bc10ce
Anton F <imantonmeep@gmail.com>
2022-07-19 20:10:02 +0200

    WIP

--- backend/src/main/kotlin/money/tegro/dex/meter/KickassMeterRegistry.kt
+++ backend/src/main/kotlin/money/tegro/dex/meter/KickassMeterRegistry.kt
@@ -6,6 +6,8 @@ import io.micrometer.core.instrument.step.StepMeterRegistry
 import io.micrometer.core.instrument.util.MeterPartition
 import io.micrometer.core.instrument.util.NamedThreadFactory
 import io.r2dbc.spi.Connection
+import kotlinx.coroutines.reactor.awaitSingleOrNull
+import kotlinx.coroutines.reactor.mono
 import mu.KLogging
 import reactor.kotlin.core.publisher.toMono
 import java.time.Instant
@@ -30,19 +32,25 @@ class KickassMeterRegistry(private val config: KickassMeterConfig, clock: Clock)
                         logger.debug { "fart, $measurement" }
                         config.connectionFactory.create().toMono()
                             .subscribe { connection: Connection ->
-                                connection.createStatement(
-                                    "INSERT INTO meter_measurements (timestamp, value, name, dimensions, metadata) VALUES ($1, $2, $3, json_object('{$4}'), json_object('{$5}'))"
-                                ).bind(0, Instant.now())
-                                    .bind(1, measurement.value)
-                                    .bind(2, meter.id.name)
-                                    .bind(3, jacksonObjectMapper().writeValueAsString(config.dimension))
-                                    .bind(
-                                        4,
-                                        meter.id.tags.map { it.key to it.value }.toMap()
-                                            .let { jacksonObjectMapper().writeValueAsString(it) })
-                                    .execute()
-                                    .toMono()
-                                    .subscribe()
+                                mono {
+                                    connection.beginTransaction().toMono().awaitSingleOrNull()
+                                    connection.createStatement(
+                                        "INSERT INTO meter_measurements (timestamp, value, name, dimensions, metadata) VALUES ($1, $2, $3, json_object('{$4,\"\"}'), json_object('{$5,\"\"}'))"
+                                    ).bind(0, Instant.now())
+                                        .bind(1, measurement.value)
+                                        .bind(2, meter.id.name)
+                                        .bind(3, jacksonObjectMapper().writeValueAsString(config.dimension))
+                                        .bind(
+                                            4,
+                                            meter.id.tags.map { it.key to it.value }.toMap()
+                                                .let { jacksonObjectMapper().writeValueAsString(it) })
+                                        .execute()
+                                        .toMono()
+                                        .flatMap { it.rowsUpdated.toMono() }
+                                        .awaitSingleOrNull()
+                                        ?.let { logger.debug { "DFLKSDJFLKSD $it" } }
+                                    connection.commitTransaction().toMono().awaitSingleOrNull()
+                                }.subscribe()
                             }
                     }
                 }
