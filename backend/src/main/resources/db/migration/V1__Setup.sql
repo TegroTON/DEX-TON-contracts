088f8694c711918a6d102748a7751d04bacbf687
Anton F <imantonmeep@gmail.com>
2022-07-19 17:43:15 +0200

    Use timestamptz to correctly handle timezones/summer time

--- backend/src/main/resources/db/migration/V1__Setup.sql
+++ backend/src/main/resources/db/migration/V1__Setup.sql
@@ -5,8 +5,8 @@ CREATE TABLE "exchange_pairs"
     "left_reserved"  NUMERIC(40) NOT NULL, /* up to 120 bits, that's like ~37 digits or so */
     "right"          BYTEA       NOT NULL,
     "right_reserved" NUMERIC(40) NOT NULL,
-    "discovered"     TIMESTAMP   NOT NULL,
-    "updated"        TIMESTAMP   NOT NULL,
+    "discovered"     TIMESTAMPTZ NOT NULL,
+    "updated"        TIMESTAMPTZ NOT NULL,
     UNIQUE ("left", "right") /* All exchange pairs must be unique */
 );
 
@@ -22,6 +22,6 @@ CREATE TABLE "jettons"
     "decimals"     INTEGER     NOT NULL,
     "image"        TEXT,
     "image_data"   BYTEA       NOT NULL,
-    "discovered"   TIMESTAMP   NOT NULL,
-    "updated"      TIMESTAMP   NOT NULL
+    "discovered"   TIMESTAMPTZ NOT NULL,
+    "updated"      TIMESTAMPTZ NOT NULL
 );
