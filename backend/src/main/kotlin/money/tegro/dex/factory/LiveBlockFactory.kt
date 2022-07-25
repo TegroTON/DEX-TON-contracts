9782e02037db59fb98b22e9061de637442ffc7e1
Anton F <imantonmeep@gmail.com>
2022-07-25 13:55:36 +0200

    Modified logic, now using only asynchronous sinks

--- backend/src/main/kotlin/money/tegro/dex/factory/LiveBlockFactory.kt
+++ backend/src/main/kotlin/money/tegro/dex/factory/LiveBlockFactory.kt
@@ -2,107 +2,105 @@ package money.tegro.dex.source
 
 import io.micrometer.core.instrument.MeterRegistry
 import io.micronaut.context.annotation.Factory
-import jakarta.inject.Singleton
+import io.micronaut.context.annotation.Prototype
+import io.micronaut.context.event.StartupEvent
+import io.micronaut.runtime.event.annotation.EventListener
+import io.micronaut.scheduling.annotation.Async
 import kotlinx.coroutines.reactor.mono
 import money.tegro.dex.config.FactoryConfig
 import mu.KLogging
 import net.logstash.logback.argument.StructuredArguments.kv
-import net.logstash.logback.argument.StructuredArguments.v
 import org.ton.api.tonnode.TonNodeBlockId
-import org.ton.api.tonnode.TonNodeBlockIdExt
 import org.ton.bigint.BigInt
 import org.ton.block.Block
-import org.ton.block.ShardDescr
 import org.ton.lite.api.LiteApi
 import reactor.core.publisher.Flux
-import reactor.kotlin.core.publisher.toFlux
+import reactor.core.publisher.Sinks
 import java.time.Duration
 
 @Factory
-class LiveBlockFactory(
+open class LiveBlockFactory(
     private val config: FactoryConfig,
     private val registry: MeterRegistry,
     private val liteApi: LiteApi
 ) {
-    private val liveMasterchainBlocks: Flux<Block> =
-        Flux.interval(Duration.ZERO, config.liveBlockPeriod)
-            .concatMap { mono { listOf(liteApi.getMasterchainInfo().last.seqno) } }
-            .distinctUntilChanged()
-            .scan { previous, current ->
-                (previous.max() + 1 until current.min()) // All missing seqnos
-                    .toList()
-                    .plus(current)
-            }
-            .flatMapIterable { it }
+    private val blocksToProcess = Sinks.many().replay().limit<TonNodeBlockId>(Duration.ofMinutes(10))
+    private val completeBlocks = Sinks.many().replay().limit<Block>(Duration.ofMinutes(10))
+
+    @Async
+    @EventListener
+    open fun setup(event: StartupEvent) {
+        // This is how we process blocks
+        blocksToProcess
+            .asFlux()
             .concatMap {
                 mono {
                     try {
-                        logger.debug("getting masterchain block no. {}", v("seqno", it))
-                        liteApi.lookupBlock(TonNodeBlockId(-1, BigInt("8000000000000000").toLong(), it))
-                            .let { liteApi.getBlock(it.id) }
-                            .toBlock()
+                        logger.debug("getting block {}", kv("id", it))
+                        liteApi.lookupBlock(it).let { liteApi.getBlock(it.id) }.toBlock()
                     } catch (e: Exception) {
-                        registry.counter("live.block.masterchain.failed").increment()
+                        registry.counter(
+                            "live.block.failed",
+                            "workchain", it.workchain.toString(),
+                            "shard", it.shard.toString()
+                        ).increment()
 
-                        logger.warn("failed to get masterchain block no. {}", v("seqno", it), e)
+                        logger.warn("failed to get block {}", kv("id", it), e)
                         null
                     }
                 }
                     .timed()
                     .map {
-                        registry.timer("live.block.masterchain.elapsed")
+                        registry.timer(
+                            "live.block.elapsed",
+                            "workchain", it.get().info.shard.workchain_id.toString(),
+                            "shard", it.get().info.shard.shard_prefix.toString()
+                        )
                             .record(it.elapsed())
                         it.get()
                     }
             }
-            .publish()
-            .autoConnect()
-
-    private val liveShardchainBlocks: Flux<Block> =
-        liveMasterchainBlocks
-            .concatMap {
-                it.extra.custom.value?.shard_hashes
+            .doOnNext { block ->
+                // Masterchain blocks also have shard_hashes, add their respective block ids to the queue
+                block.extra.custom.value?.shard_hashes
                     ?.nodes()
                     .orEmpty()
                     .flatMap {
                         val workchain = BigInt(it.first.toByteArray()).toInt()
                         it.second.nodes().map { workchain to it }
                     }
-                    .toFlux()
-                    .concatMap {
-                        mono {
-                            val (workchain, descr) = it
-                            logger.debug(
-                                "getting shard {} block no. {}", kv("workchain", workchain), v("seqno", descr.seq_no)
-                            )
-                            liteApi.getBlock(getBlockId(workchain, descr)).toBlock()
-                        }
+                    .forEach {
+                        blocksToProcess.emitNext(
+                            TonNodeBlockId(
+                                it.first,
+                                it.second.next_validator_shard,
+                                it.second.seq_no.toInt() // TODO: toInt()?
+                            ),
+                            Sinks.EmitFailureHandler.FAIL_FAST // TODO: more robust
+                        )
                     }
-                    .concatWithValues(it) // Don't forget the original masterchain block
             }
-            .publish()
-            .autoConnect()
+            .subscribe {
+                completeBlocks.emitNext(
+                    it,
+                    Sinks.EmitFailureHandler.FAIL_FAST // TODO: more robust
+                )
+            }
 
-    @Singleton
-    fun liveBlocks(): Flux<Block> =
-        Flux.merge(
-            liveMasterchainBlocks,
-            liveShardchainBlocks
-        )
-            .publish()
-            .autoConnect()
-
-    companion object : KLogging() {
-        const val MASTERCHAIN_WORKCHAIN = -1
-        val MASTERCHAIN_SHARD = BigInt("8000000000000000").toLong()
-
-        @JvmStatic
-        fun getBlockId(workchain: Int, descr: ShardDescr) = TonNodeBlockIdExt(
-            workchain = workchain,
-            shard = descr.next_validator_shard,
-            seqno = descr.seq_no.toInt(),
-            root_hash = descr.root_hash,
-            file_hash = descr.file_hash
-        )
+        // Query for the last masterchain blocks
+        Flux.interval(Duration.ZERO, Duration.ofSeconds(10))
+            .concatMap { mono { liteApi.getMasterchainInfo().last } }
+            .distinctUntilChanged()
+            .subscribe {
+                blocksToProcess.emitNext(
+                    TonNodeBlockId(it.workchain, it.shard, it.seqno),
+                    Sinks.EmitFailureHandler.FAIL_FAST // TODO: more robust
+                )
+            }
     }
+
+    @Prototype
+    fun liveBlocks(): Flux<Block> = completeBlocks.asFlux()
+
+    companion object : KLogging()
 }
